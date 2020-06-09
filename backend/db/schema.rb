fb625028c332a3f4528efcd2f7628203fd2b00bc
Michael Matich <matich.mike@gmail.com>
2020-06-08 20:10:03 -0400

    countriesDB added

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_170446) do
+ActiveRecord::Schema.define(version: 2020_06_08_230848) do
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.string "country_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "interests", force: :cascade do |t|
     t.integer "facebook_id"
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,4 +1,4 @@
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup, or migrated with rake db:migrate).
 
 require 'json'
 puts "Seeding Data ..."
@@ -7,6 +7,11 @@ def import_json(file_name)
   JSON.parse(File.read(Rails.root.join('db', file_name)))
 end
 
+def import_CountryJson(file_name)
+  JSON.parse(File.read(Rails.root.join('db', file_name)))
+end
+
+countriesData = import_json("dataCountries.json")
 interestsData = import_json("dataInterests.json")
 
 Interest.destroy_all
@@ -14,4 +19,9 @@ interestsData.each { |n|
   Interest.create(facebook_id: n["id"], name: n["name"])
 }
 
-puts "Seed imported!"
+Country.destroy_all
+countriesData.each { |n| 
+  Country.create(name: n["name"], country_code: n["country_code"])
+}
+
+puts "Seeds imported!"
