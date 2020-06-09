7b3f0efb4165485fb8fa61564fb73db23b51df7f
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-06-09 17:01:30 -0400

    Edited country functions

--- client/src/helpers/dataRequests.js
+++ client/src/helpers/dataRequests.js
@@ -5,14 +5,16 @@ const countryCodeDataSet = require('./countryCodeDataSet')
 
 // Step 2 - After the country_code has been retrieved, do a city search
 
-access_token = ENV["ACCESS_TOKEN"];
+let access_token = process.env.REACT_APP_ACCESS_TOKEN;
+let account_number = process.env.REACT_APP_ACC_NUM;
 let cityStep2 = "Paris";
 let country_code = "FR";
-let account_number = ENV["ACC_NUN"];
 
-axios.get(`https://graph.facebook.com/v7.0/search/?location_types=city&q=${cityStep2}&country_code=${country_code}&type=adgeolocation&access_token=${access_token}`).then(res => {
+axios.get(`https://graph.facebook.com/v7.0/search/?location_types=city&q=${cityStep2}&country_code=${country_code}&type=adgeolocation&access_token=${access_token}`)
+.then(res => {
   console.log("Step 2: ", res.data);
-});
+})
+.catch(res => console.log(res.error));
 
 // Step 3 - Grab the key and region_id from the city item that was selected
 
