a31769a874a0c738594f9251cbaef359fd7ca245
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-06-10 18:27:51 -0400

    Charts work

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -1,26 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Charts(props) {
+import {HorizontalBar} from 'react-chartjs-2';
+
+
+// const defaultChartsData = {
+//   labels: [],
+//   datasets: [
+//     {
+//       label: 'My First dataset',
+//       backgroundColor: 'rgba(255,99,132,0.2)',
+//       borderColor: 'rgba(255,99,132,1)',
+//       borderWidth: 1,
+//       hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+//       hoverBorderColor: 'rgba(255,99,132,1)',
+//       data: []
+//     }
+//   ]
+// };
+
+
+
+
+
+
+  const Charts = ({ reachEstimates }) => {
+  
 
   
-  console.log("Here are pps", props)
-  
 
- 
+  const defaultChartsData = {
+    labels: reachEstimates ? reachEstimates.map(item => item.interest_name) : [],
+    datasets: [
+      {
+        label: 'Audience Results',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: reachEstimates ? reachEstimates.map(item => item.data.data.users) : []
+      }
+    ]
+  };
 
-      const chartsRender = props.reachEstimates.map(function(item, i){
-        return <li key={i}>{item.interest_name} {item.data.data.users}</li>
-      })
-    
+  console.log("Charts data: ", defaultChartsData);
+
     return (
       <ul>
-        {chartsRender}
+        {/* {chartsRender} */}
+        <HorizontalBar
+          data={defaultChartsData}
+          width={100}
+          height={250}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
       </ul>
     );
-    
-
-  
-
-
-
 }
+
+export default Charts;
