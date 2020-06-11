30ac9cc523d105f15a53a32b427ecf9130b6c9cf
Michael Matich <matich.mike@gmail.com>
2020-06-10 22:35:36 -0400

    Knock Auth Create working

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :countries
+  resources :interests
   scope '/api' do
     post 'user_token' => 'user_token#create'
     post '/users' => 'users#create'
     resources :users
-    resources :countries
-    resources :interests
+    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
