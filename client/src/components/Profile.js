89d5c6a21782da720112a4a906c89745a42f09b7
Michael Matich <matich.mike@gmail.com>
2020-06-15 11:38:31 -0400

    profile styling

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import avatar from '../img/avatar.png';
 
 export default function Profile() {
   return (
     <div className="Profile-page">
+      <img className="Profile-image" src={avatar} alt="avatar" />
       <h1 className="Profile-header">Geralt of Rivia</h1>
       <span className="Profile-username">@geraltofrivia</span>
     </div>
