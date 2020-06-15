3b061bd551184047924fcb4c2c8083ff9463a77b
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-06-15 13:33:42 -0400

    Some minor front-end changes

--- client/src/App.js
+++ client/src/App.js
@@ -38,6 +38,7 @@ export default function App() {
   const [listOfInterests, setListOfInterests] = useState({})
   const [selectedInterestCategory, setSelectedInterestCategory ] = useState("family_statuses")
   const [saveObject, setSaveObject] = useState({})
+  const [chartsHeight, setChartsHeight] = useState(500)
   
   
   useEffect(() => {
@@ -329,7 +330,7 @@ export default function App() {
                 loggedIn={loggedIn}
                 /> 
               {reachEstimates.length && 
-              <Charts reachEstimates={reachEstimates} setSelectedInterestCategory={setSelectedInterestCategory} filterInterest={filterInterest} onSubmitInterest={onSubmitInterest} onSaveAudience={onSaveAudience}/>}
+              <Charts reachEstimates={reachEstimates} setSelectedInterestCategory={setSelectedInterestCategory} filterInterest={filterInterest} onSubmitInterest={onSubmitInterest} onSaveAudience={onSaveAudience} selectedInterestCategory={selectedInterestCategory} chartsHeight={chartsHeight} setChartsHeight={setChartsHeight}/>}
             </div>
             </Route>
           
